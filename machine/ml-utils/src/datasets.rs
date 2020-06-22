3a689e00c622b5134b0c7ae824dc65fb6f046cbd
teng <wwwsousoucom@126.com>
2020-06-22 23:51:43 +0800

    cargo build

--- machine/ml-utils/src/datasets.rs
+++ machine/ml-utils/src/datasets.rs
@@ -1,7 +1,7 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
-use std::fs::File;
 use std::vec::Vec;
 
 #[derive(Debug, Deserialize)]
@@ -23,25 +23,43 @@ pub struct BostonHousing {
 }
 
 impl BostonHousing {
+    // 将vec<&str>转换为架构体
     pub fn new(v: Vec<&str>) -> BostonHousing {
-        let f64_formatted: Vec<f64> = v.iter().map(
-            |s| s.parse().unwrap()
-        ).collect();
+        let f64_formatted: Vec<f64> = v.iter().map(|s| s.parse().unwrap()).collect();
         BostonHousing {
-            crim: f64_formatted[0], zn: f64_formatted[1],
-            indus: f64_formatted[2], chas: f64_formatted[3],
-            nox: f64_formatted[4], rm: f64_formatted[5],
-            age: f64_formatted[6], dis: f64_formatted[7],
-            rad: f64_formatted[8], tax: f64_formatted[9],
-            ptratio: f64_formatted[10], black: f64_formatted[11],
-            lstat: f64_formatted[12], medv: f64_formatted[13] 
+            crim: f64_formatted[0],
+            zn: f64_formatted[1],
+            indus: f64_formatted[2],
+            chas: f64_formatted[3],
+            nox: f64_formatted[4],
+            rm: f64_formatted[5],
+            age: f64_formatted[6],
+            dis: f64_formatted[7],
+            rad: f64_formatted[8],
+            tax: f64_formatted[9],
+            ptratio: f64_formatted[10],
+            black: f64_formatted[11],
+            lstat: f64_formatted[12],
+            medv: f64_formatted[13],
         }
     }
-    
+    // 转化为vec数组
     pub fn into_feature_vector(&self) -> Vec<f64> {
-        vec![self.crim, self.zn, self.indus, self.chas, self.nox,
-             self.rm, self.age, self.dis, self.rad,
-             self.tax, self.ptratio, self.black, self.lstat]
+        vec![
+            self.crim,
+            self.zn,
+            self.indus,
+            self.chas,
+            self.nox,
+            self.rm,
+            self.age,
+            self.dis,
+            self.rad,
+            self.tax,
+            self.ptratio,
+            self.black,
+            self.lstat,
+        ]
     }
 
     pub fn into_targets(&self) -> f64 {
@@ -49,16 +67,19 @@ impl BostonHousing {
     }
 }
 
+// 将一行记录转化
 fn get_boston_record(s: String) -> BostonHousing {
     let v: Vec<&str> = s.split_whitespace().collect();
     let b: BostonHousing = BostonHousing::new(v);
     b
 }
 
+// 将文件读取
 pub fn get_boston_records_from_file(filename: impl AsRef<Path>) -> Vec<BostonHousing> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
-    buf.lines().enumerate()
+    buf.lines()
+        .enumerate()
         .map(|(n, l)| l.expect(&format!("Could not parse line no {}", n)))
         .map(|r| get_boston_record(r))
         .collect()
--- machine/rustymachine-regression/src/lin_reg.rs
+++ machine/rustymachine-regression/src/lin_reg.rs
@@ -7,18 +7,6 @@ use std::path::Path;
 use std::process::exit;
 use std::vec::Vec;
 
-use rusty_machine;
-use rusty_machine::linalg::Matrix;
-// use rusty_machine::linalg::BaseMatrix;
-use rusty_machine::analysis::score::neg_mean_squared_error;
-use rusty_machine::learning::glm::{GenLinearModel, Normal};
-use rusty_machine::learning::gp::ConstMean;
-use rusty_machine::learning::gp::GaussianProcess;
-use rusty_machine::learning::lin_reg::LinRegressor;
-use rusty_machine::learning::toolkit::kernel;
-use rusty_machine::learning::SupModel;
-use rusty_machine::linalg::Vector;
-
 // use ndarray::{Array, arr1};
 use rand;
 use rand::seq::SliceRandom;
